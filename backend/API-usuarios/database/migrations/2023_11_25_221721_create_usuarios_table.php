a12383cfcd8d1a74093801a703024d9070dcf045
Jaosons <joaoalexandre05@outlook.com>
2023-11-25 21:34:16 -0300

    backend

---
+++ backend/API-usuarios/database/migrations/2023_11_25_221721_create_usuarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 12);
+            $table->string('sobrenome', 30);
+            $table->date('nascimento');
+            $table->string('cpf', 14)->unique();
+            $table->string('email', 40)->unique();
+            $table->string('senha', 30);
+            $table->string('genero', 1);
+            $table->string('estado', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
---
+++ backend/API-usuarios/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/teste','\App\Http\Controllers\Api\UsuarioController@status');
+
+Route::namespace('Api')->group(function () {
+
+    Route::post('/usuario/create', '\App\Http\Controllers\Api\UsuarioController@create');
+
+    Route::get('/usuario/read', '\App\Http\Controllers\Api\UsuarioController@read');
+    Route::get('/usuario/read/{id}', '\App\Http\Controllers\Api\UsuarioController@select');
+
+    Route::put('/usuario/update/{id}', '\App\Http\Controllers\Api\UsuarioController@update');
+
+    Route::delete('/usuario/delete/{id}', '\App\Http\Controllers\Api\UsuarioController@delete');
+});
